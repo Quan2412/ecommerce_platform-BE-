3938ee0a74cd5295e1641530060e61862bf2df63
Quan2412 <hoangthienquan5000@gmail.com>
2024-11-07 14:55:37 +0700

    Added Dashboard

--- demo/demo/src/main/java/com/ecommerce/demo/repository/UserRepository.java
+++ demo/demo/src/main/java/com/ecommerce/demo/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.ecommerce.demo.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.ecommerce.demo.dto.UserDashboardDTO;
 import com.ecommerce.demo.entity.User;
 
 @Repository
@@ -16,4 +18,32 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     
     @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM User u WHERE u.username = :username")
     boolean existsByUsername(@Param("username") String username);
+
+     @Query("""
+        SELECT NEW com.ecommerce.demo.dto.UserDashboardDTO(
+            u.username,
+            COUNT(o.id) as totalPurchases,
+            p1.name as mostBoughtProduct,
+            p2.name as mostSoldProduct,
+            SUM(o.totalAmount) as totalRevenue)
+        FROM User u
+        LEFT JOIN Order o ON u.id = o.userId
+        LEFT JOIN (
+            SELECT op.product.name, COUNT(op) as buyCount
+            FROM OrderProduct op
+            GROUP BY op.product.name
+            ORDER BY buyCount DESC
+            LIMIT 1
+        ) p1
+        LEFT JOIN (
+            SELECT sp.product.name, COUNT(sp) as sellCount
+            FROM SellerProduct sp
+            GROUP BY sp.product.name
+            ORDER BY sellCount DESC
+            LIMIT 1
+        ) p2
+        GROUP BY u.id, u.username
+        ORDER BY totalRevenue DESC
+    """)
+    List<UserDashboardDTO> findUserDashboardStats();
 }
